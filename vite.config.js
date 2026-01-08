76d72ee11e66114d35ad44db0e6ec48fc8c857e5
shivansh330 <shiv300124@gmail.com>
2026-01-08 19:48:00 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,26 +1,26 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: "/",
   plugins: [react()],
   server: {
     port: 3000,
     open: true
   },
   build: {
-    outDir: 'dist',
+    outDir: "dist",
     sourcemap: true
   },
   define: {
-    global: 'globalThis',
+    global: "globalThis"
   },
   resolve: {
     alias: {
       process: "process/browser",
       stream: "stream-browserify",
       zlib: "browserify-zlib",
-      util: 'util'
+      util: "util"
     }
   }
-})
+});
